dc34e19721a6c03f41502eb0f917e12d30cf0712
JcPerera <jcperera.online@gmail.com>
2022-01-25 08:11:30 +1100

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import Cart from './pages/Cart';
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Product from './pages/Product';
-import ProductList from './pages/ProductList';
-
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
     </div>
   );
 }
